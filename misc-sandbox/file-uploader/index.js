9a2258ebacb7e8bb3e6c53532d9937d82a687ee3
Dmitry Korlas <dmitry.korlas@gmail.com>
2015-04-30 13:04:54 +0300

    ajax uploader: update demo backend, compatibility with ie8

--- misc-sandbox/file-uploader/index.js
+++ misc-sandbox/file-uploader/index.js
@@ -1,12 +1,32 @@
 // Implements basic http server
 var express = require('express');
 var serveStatic = require('serve-static');
+var multer  = require('multer');
+
 
 var app = express();
 
+var FAKE_DELAY = 2000;
+
 app.use(serveStatic(__dirname + '/www'));
+app.use(multer({ dest: './uploads/'}));
+
 app.post('/upload', function(req, res){
-	res.send('Upload complete');
+
+	//console.log(req.files);
+	//console.log('body');
+	//console.log(req.body);
+
+	var fieldName = 'files[]';
+	setTimeout(function(){
+		var file = req.files[fieldName];
+		var msg = "Upload complete.\n";
+		msg += "originalName:"+ file.originalname + ', size:'+ file.size;
+
+		res.send(msg);
+
+	}, FAKE_DELAY);
+
 });
 
-app.listen(3000);
+app.listen(3001);
